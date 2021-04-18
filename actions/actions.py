aa0a8eaafca8afbb2fe4c6cb97efd049732fa77e
Leao Liu <llm2@cin.ufpe.br>
2021-04-18 15:43:51 -0300

    Add second action joke

--- actions/actions.py
+++ actions/actions.py
@@ -40,3 +40,16 @@ class ActionJoke(Action):
         joke = request['value']['joke']  # extract a joke
         dispatcher.utter_message(text=joke)  # send the message back
         return []
+
+
+class ActionJoke2(Action):
+    def name(self):
+        return "action_joke2"
+
+    def run(self, dispatcher, tracker, domain):
+        request = requests.get(
+            'http://api.icndb.com/jokes/random'
+        ).json()  # make an api call
+        joke = request['value']['joke']  # extract a joke
+        dispatcher.utter_message(text=joke)  # send the message back
+        return []
