95a1ef6680d33122f7c5c3804895194568284d0f
Leao Liu <llm2@cin.ufpe.br>
2021-04-18 18:37:53 -0300

    remove joke 2

--- actions/actions.py
+++ actions/actions.py
@@ -42,14 +42,3 @@ class ActionJoke(Action):
         return []
 
 
-class ActionJoke2(Action):
-    def name(self):
-        return "action_joke2"
-
-    def run(self, dispatcher, tracker, domain):
-        request = requests.get(
-            'http://api.icndb.com/jokes/random'
-        ).json()  # make an api call
-        joke = request['value']['joke']  # extract a joke
-        dispatcher.utter_message(text=joke)  # send the message back
-        return []
