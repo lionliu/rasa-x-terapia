03ad9b0b7a96918ff7a852b1bf0ba9316150b9c9
Filipe Cumaru <facsa@cin.ufpe.br>
2021-04-21 18:41:55 -0300

    feat: notify back-end when a new conversation is started

--- actions/actions.py
+++ actions/actions.py
@@ -28,7 +28,11 @@
 
 import requests
 import json
-from rasa_sdk import Action
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet, SessionStarted, ActionExecuted, EventType
+
+
 class ActionJoke(Action):
     def name(self):
         return "action_joke"
@@ -42,3 +46,21 @@ class ActionJoke(Action):
         return []
 
 
+class ActionSessionStart(Action):
+    def name(self) -> Text:
+        return "action_session_start"
+
+    async def run(self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # the session should begin with a `session_started` event
+        events = [SessionStarted()]
+        events.extend(tracker.slots)
+
+        request = await requests.post(
+            "https://terapiabot.herokuapp.com/users",
+            {
+                "conversationId": tracker.sender_id,
+                "name": tracker.sender_id
+            }
+        ).json()
+
+        return events
