8201367b6c0766563d06a1d9e77e40a86fae08c4
Filipe Cumaru <facsa@cin.ufpe.br>
2021-04-21 21:25:22 -0300

    feat(actions): new action to sync bot with user's todoist account

--- actions/actions.py
+++ actions/actions.py
@@ -28,6 +28,7 @@
 
 import requests
 import json
+from uuid import uuid4
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet, SessionStarted, ActionExecuted, EventType
@@ -50,17 +51,30 @@ class ActionSessionStart(Action):
     def name(self) -> Text:
         return "action_session_start"
 
-    async def run(self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         # the session should begin with a `session_started` event
         events = [SessionStarted()]
         events.extend(tracker.slots)
 
-        request = await requests.post(
-            "https://terapiabot.herokuapp.com/users",
-            {
-                "conversationId": tracker.sender_id,
-                "name": tracker.sender_id
-            }
-        ).json()
+        requests.post("https://terapiabot.herokuapp.com/users",
+                      {"conversationId": tracker.sender_id, "name": tracker.sender_id})
+
+        dispatcher.utter_message(
+            text="Para sincronizar à sua conta do Todoist digite /action_sync_todoist a qualquer momento durante nossa conversa!")
 
         return events
+
+
+class ActionSyncWithTodoist(Action):
+    def name(self) -> Text:
+        return "action_sync_todoist"
+
+    def run(self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        todoist_state = str(uuid4())
+        todoist_sync_url = "https://todoist.com/oauth/authorize?client_id=1164292444224bdeb3de134a34ecf016&scope=data:read&state={}".format(
+            todoist_state)
+        
+        dispatcher.utter_message(
+            text="OK! Preciso que você entre neste link e me forneça autorização para saber quando você tiver alguma tarefa:\n {}".format(todoist_sync_url))
+        
+        return []
